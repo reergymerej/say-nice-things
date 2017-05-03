85e45d74e08c981bf4ccd34b8d8434b8bee65c4c
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-02 20:39:46 -0500

    randomly select message

--- say-nice-things.cpp
+++ say-nice-things.cpp
@@ -1,19 +1,26 @@
 // This is a command line program that will say nice things to you.
 #include <iostream>
 #include <string>
+#include <time.h>
 using namespace std;
 
 int main()
 {
-  string compliments [5];
+  int complimentsLength = 5;
+  string compliments [complimentsLength];
   compliments[0] = "You look handsome!";
   compliments[1] = "Today is going to be great!";
   compliments[2] = "I'm happy you're here.";
   compliments[3] = "Life is the best game ever played.";
   compliments[4] = "You're family is rad.";
 
-  for (int i = 0; i < 5; i++) {
-    cout << compliments[i] << endl;
-  }
+  // Which will we choose?
+  // seed randomizer
+  srand(time(NULL));
+  // get some randomness
+  int index = rand() % complimentsLength;
+
+  cout << compliments[index] << endl;
+
   return 0;
 }
