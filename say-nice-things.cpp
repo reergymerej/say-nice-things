0b3b2d8f76e3586a28103314ca891d33a99b9289
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-02 22:22:58 -0500

    use vector instead of array for compliments

--- say-nice-things.cpp
+++ say-nice-things.cpp
@@ -2,23 +2,31 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <vector>
 using namespace std;
+
+int getRandomInt(int max)
+{
+  srand(time(NULL));
+  return rand() % (max + 1);
+}
+
+vector<string> getCompliments(int length)
+{
+  vector<string> compliments;
+  compliments.push_back("You look handsome!");
+  compliments.push_back("Today is going to be great!");
+  compliments.push_back("I'm happy you're here.");
+  compliments.push_back("Life is the best game ever played.");
+  compliments.push_back("You're family is rad.");
+  return compliments;
+}
 
 int main()
 {
   int complimentsLength = 5;
-  string compliments [complimentsLength];
-  compliments[0] = "You look handsome!";
-  compliments[1] = "Today is going to be great!";
-  compliments[2] = "I'm happy you're here.";
-  compliments[3] = "Life is the best game ever played.";
-  compliments[4] = "You're family is rad.";
-
-  // Which will we choose?
-  // seed randomizer
-  srand(time(NULL));
-  // get some randomness
-  int index = rand() % complimentsLength;
+  vector<string> compliments = getCompliments(complimentsLength);
+  int index = getRandomInt(complimentsLength - 1);
 
   cout << compliments[index] << endl;
 
